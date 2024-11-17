a1bb80e90de1b98d482eb1d49b0f2d3a6bd8c4ba
sanchi <goyalsanchi0111@gmail.com>
2024-11-17 12:39:33 +0530

    redo

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import "./Hero.css";
-import banner1 from "../Assets/banner1.jpg";
-/*import hand_icon from "../Assets/hand_icon.png";
-import arrow_icon from "../Assets/arrow.png";*/
+import hero_image from "../Assets/hero_image.png";
+import hand_icon from "../Assets/hand_icon.png";
+import arrow_icon from "../Assets/arrow.png";
 
 const Hero = () => {
   return (
     <div className="hero">
-     
-      <img src={banner1} alt="hero" />
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+          <div className="hero-hand-icon">
+            <p>new</p>
+            <img src={hand_icon} alt="" />
+          </div>
+          <p>collections</p>
+          <p>for everyone</p>
+        </div>
+        <div className="hero-latest-btn">
+          <div>Latest Collection</div>
+          <img src={arrow_icon} alt="" />
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src={hero_image} alt="hero" />
+      </div>
     </div>
   );
 };
