ce6a2ddd5482d69b764cf40e45d27f316f2c9ff9
sanchi <goyalsanchi0111@gmail.com>
2024-11-17 16:42:52 +0530

    front page

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./Hero.css";
 
-import hand_icon from "../Assets/hand_icon.png";
-import arrow_icon from "../Assets/arrow.png";
 import new_banner from "../Assets/new_banner.jpg"
 const Hero = () => {
   return (
